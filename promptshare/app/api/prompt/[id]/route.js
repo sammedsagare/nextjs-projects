4ba129fc5fb3487ee846c9ba7beac82f0b918f8a
Sammed Sagare <76863715+sammedsagare@users.noreply.github.com>
2024-01-31 12:28:38 +0530

    created my profile, edit, delete feature

---
+++ promptshare/app/api/prompt/[id]/route.js
@@ -0,0 +1,84 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+//get to read prompt
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDB();
+
+    const prompt = await Prompt.findById(params.id).populate("creator");
+    if (!prompt) {
+      return new Response("Prompt not found.", {
+        status: 404,
+        statusText: "Prompt not found",
+      });
+    }
+    return new Response(JSON.stringify(prompt), {
+      status: 200,
+      statusText: "Prompts fetched",
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Failed to fetch prompts.", {
+      status: 500,
+      statusText: "Internal server error",
+    });
+  }
+};
+
+//patch to update prompt
+
+export const PATCH = async (req, { params }) => {
+  const { prompt, tag } = await req.json();
+
+  try {
+    await connectToDB();
+
+    const existingPrompt = await Prompt.findByIdAndUpdate(params.id, {
+      prompt,
+      tag,
+    });
+
+    if (!prompt) {
+      return new Response("Prompt not found.", {
+        status: 404,
+        statusText: "Prompt not found",
+      });
+    }
+
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+    await existingPrompt.save();
+
+    return new Response("Prompt edited successfully.", {
+      status: 200,
+      statusText: "Prompt edited",
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Failed to update prompt.", {
+      status: 500,
+      statusText: "Internal server error",
+    });
+  }
+};
+
+//delete to delete prompt
+export const DELETE = async (req, { params }) => {
+  try {
+    await connectToDB();
+
+    await Prompt.findByIdAndDelete(params.id);
+
+    return new Response("Prompt deleted successfully.", {
+      status: 200,
+      statusText: "Prompt deleted",
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Failed to delete prompt.", {
+      status: 500,
+      statusText: "Internal server error",
+    });
+  }
+};
